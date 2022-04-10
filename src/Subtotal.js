a701cef8d902309a18101e9d9a922fd1cb8be7d6
Elijah-A-W <48324239+Elijah-A-W@users.noreply.github.com>
2022-04-10 18:20:01 +0300

    Added routing on the Proceed to checkout button.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,10 +3,15 @@ import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from './StateProvider';
 import { getBasketTotal  } from "./reducer";
+import { useNavigate } from "react-router-dom";
+
 
 
 function Subtotal() {
+
+  const navigate = useNavigate();
   const [{ basket }, dispatch] = useStateValue();
+
 
   return (
     <div className="subtotal">
@@ -28,7 +33,7 @@ function Subtotal() {
                   prefix={"$"}
                 />         
 
-            <button>Proceed to Checkout</button>
+            <button onClick={e => navigate('/Payment')}>Proceed to Checkout</button>
     </div>
   )
 }
